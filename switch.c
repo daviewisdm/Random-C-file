3a1d65cfd4659f53090a062abda397a378356370
Davie Kibet <wisdomdavie24@gmail.com>
2023-06-30 10:10:22 +0300

    Zeraki lite

---
+++ switch.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    // Define the variables
+    char grade;
+    // Get the user's grade
+    printf("Enter your grade: ");
+    scanf(" %c ", &grade);
+    // Check the grade and output the remarks
+    switch (grade)
+    {
+    case 'A':
+        printf("Excellent!");
+        break;
+    case 'B':
+        printf("Good!");
+        break;
+    case 'C':
+        printf("Average!");
+        break;
+    case 'D':
+        printf("Needs improvement!");
+        break;
+    case 'F':
+        printf("Fail!");
+        break;
+    default:
+        printf("Invalid grade!");
+        break;
+    }
+    return 0;
+}
